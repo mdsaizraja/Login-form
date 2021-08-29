24d52cab8fe49c9b134147810dc563f3f4ad4172
mdsaizraja <mdraja883@gmail.com>
2021-08-29 09:45:02 +0530

    js

---
+++ login.js
@@ -0,0 +1,30 @@
+const usernameInput = document.querySelector('input[type="text"]');
+usernameInput.addEventListener("input", function(e){
+  e.target.value = e.target.value.toLowerCase();
+});
+
+
+
+function myFunction(){
+    var x = document.getElementById("myInput");
+    var y = document.getElementById("hide1");
+    var z = document.getElementById("hide2");
+    if(x.type === 'password'){
+
+    
+    x.type = 'text';
+    y.style.display = "block";
+    z.style.display = "none";
+
+
+ }
+
+ else{
+    x.type = "password";
+    y.style.display = "none";
+    z.style.display = "block";
+
+
+ }
+
+}
